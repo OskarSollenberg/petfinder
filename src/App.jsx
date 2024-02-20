12a866d49dfaf0bd2a2c2c4b2e500d6dddea74e6
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-20 21:26:07 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+import Products from "./components/Products";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Products />
+      <Products />
+    </>
+  );
+}
