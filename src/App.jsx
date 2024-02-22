dfe4ed28f84a25d9bd5d62c23555189f1b24317f
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-22 15:13:08 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ export default function App() {
         <Process />
         <AboutUs />
         <Reviews />
-        <Footer />
         {/* <Popup /> */}
       </main>
+      <Footer />
     </>
   );
 }
