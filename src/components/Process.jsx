beef6e7915fb2e12a548179bb875a42a19279074
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-22 16:29:55 +0100

    .

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import {
-  TbHexagonNumber1,
-  TbHexagonNumber2,
-  TbHexagonNumber3,
-  TbHexagonNumber4,
-  TbHexagonNumber5,
-  TbHexagonNumber6,
-} from "react-icons/tb";
 
 export default function Process() {
+  function Steps() {
+    const stepsInfo = [
+      {
+        id: 1,
+        title: "This is the first step",
+        description:
+          "We will start with the first step and move on to the next step.",
+      },
+      {
+        id: 2,
+        title: "This is the second step",
+        description:
+          "We will start with the first step and move on to the next step.",
+      },
+      {
+        id: 3,
+        title: "This is the third step",
+        description:
+          "We will start with the first step and move on to the next step.",
+      },
+      {
+        id: 4,
+        title: "This is the fourth step",
+        description:
+          "We will start with the first step and move on to the next step.",
+      },
+    ];
+    return stepsInfo.map((step) => (
+      <div className="md:max-w-[15rem] border p-6 rounded-xl shadow-sm bg-white">
+        <div className="flex items-center justify-center w-14 h-14 bg-[#1354FD] rounded-full">
+          <span className="text-white text-[1.5rem]">{step.id}</span>
+        </div>
+        <h3 className="text-xl font-bold mt-4">{step.title}</h3>
+        <p className="text-gray-500 mt-2">{step.description}</p>
+      </div>
+    ));
+  }
+
   return (
-    <section className="h-[30rem] flex justify-center items-center border-black border-2 mb-10">
-      Process Section
+    <section className=" p-10 border mb-10 flex justify-around flex-col items-center gap-[8rem]">
+      <div>
+        <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Our Simple Process
+        </h2>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <Steps />
+      </div>
     </section>
   );
 }
