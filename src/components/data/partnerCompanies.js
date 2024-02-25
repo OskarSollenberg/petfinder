e518edff8d63812080ccd38adfbce722b86d63c3
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-25 20:03:05 +0100

    add - name partner

--- src/components/data/partnerCompanies.js
+++ src/components/data/partnerCompanies.js
@@ -4,7 +4,7 @@ const partnerCompanies = [
       logoPartner: 'https://www.purina.se/themes/custom/trim_the_tail_th/logo-og.png'
     },
     {
-      namePartner: 'If-insurance',
+      namePartner: 'If',
       logoPartner: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/If_P%26C_Insurance_logo.svg/800px-If_P%26C_Insurance_logo.svg.png'
     },
     {
