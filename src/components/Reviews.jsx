9be20f01618e6576cea1b420fcc0d015247c334f
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-25 19:14:16 +0100

    fix - reviews component size

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,7 @@ import Star from './Stars.jsx';
 export default function Reviews() {
   return (
     <section>
-      <div className='mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16'>
+      <div className='mx-auto max-w-screen-xl py-12 sm:px-6 lg:px-8 lg:py-16'>
         <h2 className='text-left text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
           Read trusted reviews from our pet lovers
         </h2>
@@ -13,7 +13,7 @@ export default function Reviews() {
           {reviewsUsers.map((review, index) => (
             <article key={index} className='mt-4'>
               <div className='mb-4 sm:break-inside-avoid'>
-                <blockquote className='rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8'>
+                <blockquote className='rounded-lg bg-gray-50 p-2 sm:p-6 shadow-sm md:p-8'>
                   <div className='flex items-center gap-4'>
                     <img
                       alt={`Profile picture of ${review.nameUser}`}
