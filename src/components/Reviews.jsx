48bf12452ea46ce6f0da9c8732cbb88d7c1a5286
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-25 14:02:08 +0100

    update commit

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,7 @@ export default function Reviews() {
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-        <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h2 className="text-left text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Read trusted reviews from our pet lovers
         </h2>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
