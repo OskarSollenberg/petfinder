0498917b58f47b197b23ce0f29d5a6e7a50de376
Alexander Heras <alexander.heras@hotmail.com>
2024-02-27 10:21:56 +0100

    Edited aria label in pet section

--- src/components/Pets.jsx
+++ src/components/Pets.jsx
@@ -3,15 +3,14 @@ import { pets } from "../data/petData";
 
 export default function Pets() {
   return (
-    <section className="bg-white">
+    <section className="bg-white" aria-label="pets-section">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col gap-10">
         <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Pets For Adoption
         </h2>
-
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {pets.map((pet) => (
-            <a href="#" key={pet.id} className="group">
+            <a href="#" key={pet.id} className="group" aria-label={`link to ${pet.name}, ${pet.breed}, ${pet.age}`}>
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
                   src={pet.imageSrc}
@@ -19,20 +18,10 @@ export default function Pets() {
                   className="h-full w-full object-cover object-center group-hover:opacity-75"
                 />
               </div>
-              <span className="sr-only">
-                Name of pet: {pet.name}, Breed of pet: {pet.breed}, Age of pet:{" "}
-                {pet.age}
-              </span>
-              <h3
-                className="mt-4 text-xl text-gray-700 font-bold"
-                aria-hidden="true"
-              >
+              <h3 className="mt-4 text-xl text-gray-700 font-bold">
                 {pet.name}
               </h3>
-              <p
-                className="mt-1 text-sm font-medium text-gray-900"
-                aria-hidden="true"
-              >
+              <p className="mt-1 text-sm font-medium text-gray-900">
                 {pet.breed}, {pet.age}
               </p>
             </a>
